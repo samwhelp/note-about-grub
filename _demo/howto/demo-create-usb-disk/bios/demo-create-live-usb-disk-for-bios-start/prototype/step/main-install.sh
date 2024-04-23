ec7f1026d66a8c68c327a68e06b811089903a136
samwhelp <samwhelp@users.noreply.github.com>
2024-04-23 17:49:27 +0800

    fix

---
+++ _demo/howto/demo-create-usb-disk/bios/demo-create-live-usb-disk-for-bios-start/prototype/step/main-install.sh
@@ -0,0 +1,211 @@
+#!/usr/bin/env bash
+
+
+################################################################################
+### Head: Note
+##
+
+##
+## ## Link
+##
+## * https://github.com/samwhelp/skel-project-plan/blob/master/develop/sh/project-helper/basic/helper/bin/prepare.sh
+##
+
+##
+### Tail: Note
+################################################################################
+
+
+################################################################################
+### Head: Init
+##
+
+#set -e
+
+THE_BASE_DIR_PATH="$(cd -- "$(dirname -- "${0}")" ; pwd)"
+#THE_INIT_DIR_PATH="${THE_BASE_DIR_PATH}/helper/ext"
+#. "${THE_INIT_DIR_PATH}/init.sh"
+
+##
+### Tail: Init
+################################################################################
+
+
+
+
+
+
+################################################################################
+### Head: Util / Debug
+##
+
+util_debug_echo () {
+
+	if is_debug; then
+		echo "${@}" 1>&2
+	fi
+
+}
+
+util_error_echo () {
+
+	echo "${@}" 1>&2
+
+}
+
+##
+### Head: Util / Debug
+################################################################################
+
+
+
+################################################################################
+### Head: Base
+##
+
+##
+## THE_BASE_DIR_PATH="$(cd -- "$(dirname -- "$0")" ; pwd)"
+##
+
+find_dir_path () {
+
+	if ! [ -d "$(dirname -- "${1}")" ]; then
+		dirname -- "${1}"
+		return 1
+	fi
+	echo "$(cd -- "$(dirname -- "${1}")" ; pwd)"
+
+}
+
+##
+## THIS_BASE_DIR_PATH="$(find_dir_path "${0}")"
+##
+
+
+##
+## $ export DEBUG_HELPER=true
+##
+
+is_debug () {
+
+	if [ "${DEBUG_HELPER}" = "true" ]; then
+		return 0
+	fi
+
+	return 1
+
+}
+
+is_not_debug () {
+
+	! is_debug
+
+}
+
+##
+### Tail: Base
+################################################################################
+
+
+
+################################################################################
+### Head: Model / live_usb_disk_for_bios_main_install
+##
+
+live_usb_disk_for_bios_main_install () {
+
+	util_error_echo
+	util_error_echo "##"
+	util_error_echo "## ## live_usb_disk_for_bios_main_install"
+	util_error_echo "##"
+	util_error_echo
+
+
+	mod_main_install_for_bios
+
+	return 0
+}
+
+
+mod_main_install_for_bios () {
+
+	mod_main_install_for_bios_mount
+
+	mod_main_install_for_bios_grub_install
+
+	return 0
+
+}
+
+mod_main_install_for_bios_mount () {
+
+
+	mkdir -p ./mnt
+
+	sudo umount ./mnt
+
+	sudo mount /dev/sdc2 ./mnt
+
+
+	return 0
+
+}
+
+mod_main_install_for_bios_grub_install () {
+
+
+	sudo grub-install \
+		--boot-directory=mnt/boot \
+		--target=i386-pc \
+		--fonts=unicode \
+		/dev/sdc
+
+
+	return 0
+
+}
+
+
+##
+### Tail: Model / live_usb_disk_for_bios_main_install
+################################################################################
+
+
+
+################################################################################
+### Head: Model / Start
+##
+
+main_usb_disk_create () {
+
+	#util_error_echo 'main_usb_disk_create'
+
+	live_usb_disk_for_bios_main_install
+
+
+
+
+	return 0
+
+}
+
+##
+### Tail: Model / Start
+################################################################################
+
+
+################################################################################
+### Head: Main
+##
+
+__main__ () {
+
+	main_usb_disk_create "${@}"
+
+}
+
+__main__ "${@}"
+
+##
+### Tail: Main
+################################################################################
---
+++ _demo/howto/demo-create-usb-disk/uefi/demo-create-live-usb-disk-for-uefi-start/prototype/step/main-install.sh
@@ -0,0 +1,188 @@
+#!/usr/bin/env bash
+
+
+################################################################################
+### Head: Note
+##
+
+##
+## ## Link
+##
+## * https://github.com/samwhelp/skel-project-plan/blob/master/develop/sh/project-helper/basic/helper/bin/prepare.sh
+##
+
+##
+### Tail: Note
+################################################################################
+
+
+################################################################################
+### Head: Init
+##
+
+#set -e
+
+THE_BASE_DIR_PATH="$(cd -- "$(dirname -- "${0}")" ; pwd)"
+#THE_INIT_DIR_PATH="${THE_BASE_DIR_PATH}/helper/ext"
+#. "${THE_INIT_DIR_PATH}/init.sh"
+
+##
+### Tail: Init
+################################################################################
+
+
+
+
+
+
+################################################################################
+### Head: Util / Debug
+##
+
+util_debug_echo () {
+
+	if is_debug; then
+		echo "${@}" 1>&2
+	fi
+
+}
+
+util_error_echo () {
+
+	echo "${@}" 1>&2
+
+}
+
+##
+### Head: Util / Debug
+################################################################################
+
+
+
+################################################################################
+### Head: Base
+##
+
+##
+## THE_BASE_DIR_PATH="$(cd -- "$(dirname -- "$0")" ; pwd)"
+##
+
+find_dir_path () {
+
+	if ! [ -d "$(dirname -- "${1}")" ]; then
+		dirname -- "${1}"
+		return 1
+	fi
+	echo "$(cd -- "$(dirname -- "${1}")" ; pwd)"
+
+}
+
+##
+## THIS_BASE_DIR_PATH="$(find_dir_path "${0}")"
+##
+
+
+##
+## $ export DEBUG_HELPER=true
+##
+
+is_debug () {
+
+	if [ "${DEBUG_HELPER}" = "true" ]; then
+		return 0
+	fi
+
+	return 1
+
+}
+
+is_not_debug () {
+
+	! is_debug
+
+}
+
+##
+### Tail: Base
+################################################################################
+
+
+
+################################################################################
+### Head: Model / live_usb_disk_for_uefi_main_install
+##
+
+live_usb_disk_for_uefi_main_install () {
+
+	util_error_echo
+	util_error_echo "##"
+	util_error_echo "## ## live_usb_disk_for_uefi_main_install"
+	util_error_echo "##"
+	util_error_echo
+
+
+	mod_main_install_for_uefi
+
+	return 0
+}
+
+
+mod_main_install_for_uefi () {
+
+	mod_main_install_for_uefi_mount
+
+	return 0
+
+}
+
+mod_main_install_for_uefi_mount () {
+
+
+	return 0
+
+}
+
+
+##
+### Tail: Model / live_usb_disk_for_uefi_main_install
+################################################################################
+
+
+
+################################################################################
+### Head: Model / Start
+##
+
+main_usb_disk_create () {
+
+	#util_error_echo 'main_usb_disk_create'
+
+	live_usb_disk_for_uefi_main_install
+
+
+
+
+	return 0
+
+}
+
+##
+### Tail: Model / Start
+################################################################################
+
+
+################################################################################
+### Head: Main
+##
+
+__main__ () {
+
+	main_usb_disk_create "${@}"
+
+}
+
+__main__ "${@}"
+
+##
+### Tail: Main
+################################################################################
